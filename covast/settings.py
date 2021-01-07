899e901cd125fef3c5eed8a40783ea133d67a31d
leensungzero <mystar09070907@gmail.com>
2021-01-07 21:49:57 +0900

    Register app  (inoculation, member, post)

--- covast/settings.py
+++ covast/settings.py
@@ -52,6 +52,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'inoculation',
+    'member',
+    'post',
 ]
 
 MIDDLEWARE = [
