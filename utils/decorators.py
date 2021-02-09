ac8ba7dce97ef1f62c1bf9ba85354b47a555882e
leensungzero <31237832+leensungzero@users.noreply.github.com>
2021-02-09 20:08:51 +0900

    Resolve conflict (#7)
    * Make hospital_data_setup command
    
    * Add (requests, xmltoddict) library
    
    * Add (COVID19_HOSPITAL_SERVICE_KEY, KAKAO_REST_API_KEY) key
    
    * Register (COVID19_HOSPITAL_SERVICE_KEY, KAKAO_REST_API_KEY) to settings file
    
    * Resolve conflict
    
    * Error code fix
    
    * Create command 'hospital_data_setup'
    
    * Resolve conflict
    
    * Resolve conflict
    
    * chore: update gitignore
    
    * chore: add package for postgresql
    
    * fix: json format error
    
    * need rebase
    
    * chore: create secrets util py and update project setting
    
    * chore: dockerize
    
    * chore: create github action workflow to continuous deployment
    
    * docs: add docker contents to readme
    
    Co-authored-by: leensungzero <mwkang@humelo.com>
    Co-authored-by: gtg7784 <tae.gun7784@gmail.com>

--- utils/decorators.py
+++ utils/decorators.py
@@ -8,7 +8,11 @@ def login_required(func):
         try:
             key = kwargs.get('token')
             token_obj = Token.objects.get(key=key)
-            info.context.user = token_obj.user
+            if token_obj.user.is_authenticated:
+                info.context.user = token_obj.user
+            else:
+                raise Token.DoesNotExist
+
         except Token.DoesNotExist:
             raise ValidationError('Authentication failed')
         except Exception as e:
---
+++ utils/secrets.py
@@ -0,0 +1,18 @@
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
